c5de86c5567d358928ccedddfdea9bf9c2789a36
shivam66151 <sharmashivam66151@gmail.com>
2022-01-15 23:22:37 +0530

    1 file modified

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const hours = document.getElementById('hours');
 const mins = document.getElementById('min');
 const secs = document.getElementById('sec');
 
-const newYear = 'January 1' + (getcurrentyear + 1);
+const newYear = 'January 1' + (new Date().getFullYear() + 1);
 
 function countdown() {
-  const getcurrentyear = new Date().getFullYear();
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
   const totalSeconds = (newYearsDate - currentDate) / 1000;
 
   const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const secs = 
 }
