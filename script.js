e38a361e46412fa331a88b441ff6dc0c9db27522
shivam66151 <sharmashivam66151@gmail.com>
2022-01-15 23:57:43 +0530

    2 files modified

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursE1 = document.getElementById('hours');
 const minsE1 = document.getElementById('min');
 const secsE1 = document.getElementById('sec');
 
-const newYear = '1 Jan 2022';
+const newYears = '1 Jan 2023';
 
 function countdown() {
   const newYearsDate = new Date(newYears);
@@ -23,9 +23,10 @@ function countdown() {
 }
 
 function formatTime(time) {
-  return time < 10 ? '0${time}' : time;
+  return time < 10 ? `0${time}` : time;
 }
 
+//
 countdown();
 
 setInterval(countdown, 1000);
